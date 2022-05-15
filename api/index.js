9f29c01c4a2673e7c03a2d7eb94d34005f54cd30
John Matthew Flores <joflo.matres@gmail.com>
2022-05-15 20:45:23 +0800

    update mock server

---
+++ api/index.js
@@ -0,0 +1,20 @@
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('api/robots.json')
+const middlewares = jsonServer.defaults()
+
+// Set default middlewares (logger, static, cors and no-cache)
+server.use(middlewares)
+
+// Add custom routes before JSON Server route
+server.post('/robots/:version/extinguish', function (req, res) {
+// See https://github.com/typicode/lowdb
+    req.method = "GET"
+    res.jsonp(req.query)
+})
+
+// Use default router
+server.use(router)
+server.listen(3000, () => {
+  console.log('JSON Server is running')
+})
